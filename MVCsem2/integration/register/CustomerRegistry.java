5acd6857150c35e65deddc913dded41cefc33d99
Ahmednuur Ahmed <ahahme@kth.se>
2026-04-25 14:02:18 +0200

    fixed integration/registers

--- MVCsem2/integration/register/CustomerRegistry.java
+++ MVCsem2/integration/register/CustomerRegistry.java
@@ -1,17 +1,37 @@
 package MVCsem2.integration.register;
 
+import MVCsem2.model.entity.Bike;
 import MVCsem2.model.entity.Customer;
 
+/**
+ *  This class handles stored customer data 
+ */
 public class CustomerRegistry {
+    private final Customer[] customers;
 
-	private Customer customer;
+    /**
+     * Creates a customer registry with hardcoded data cause we dont have a database
+     */
+    public CustomerRegistry() {
+        customers = new Customer[] {
+            new Customer("Ahmed Adam", "ahmed@mail.com", "0701234567",
+                    new Bike("Abdurraham", "Reaction Hybrid", "CUBE123")),
+            new Customer("Sara Svensson", "sara@mail.com", "0737654321",
+                    new Bike("Trek", "Powerfly 5", "TREK456"))
+        };
+    }
 
-	public Customer findCustomer(String phoneNumber) {
-		return null;
-	}
-
-	public CustomerRegistry CustomerRegistry() {
-		return null;
-	}
-
+    /**
+     * Uses a customers phone number as a parameter for look up in the hardcoded database.
+     * @param phoneNumber The phone number used to identify the customer.
+     * @return The customer with the given phone number, null if the customer was not found.
+     */
+    public Customer findCustomer(String phoneNumber) {
+       for (int i = 0; i < customers.length; i++) {
+    		if (customers[i].getPhoneNumber().equals(phoneNumber)) {
+        		return customers[i];
+    		}
+		}
+        return null;
+    }
 }
