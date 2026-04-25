1251b81c0fbb8e892854a824a2cfdc56544a317e
Ahmednuur Ahmed <ahahme@kth.se>
2026-04-25 14:42:40 +0200

    fixed RepairOrderRegistry

--- MVCsem2/integration/register/RepairOrderRegistry.java
+++ MVCsem2/integration/register/RepairOrderRegistry.java
@@ -6,18 +6,16 @@ import MVCsem2.model.entity.RepairOrder;
  * Handles access and storage of repair orders in the Bike Repair Shop.
  */
 public class RepairOrderRegistry {
-    private final RepairOrder[] repairOrders;
+    private RepairOrder[] repairOrders;
 
     /**
-     * Creates a registry with hardcoded repair orders instead of using a real database.
+     * Creates a registry with hardcoded repair orders, instead of using a real database.
      */
     public RepairOrderRegistry() {
         repairOrders = new RepairOrder[] {
-
-            new RepairOrder("RO10SDFQ"),
-            new RepairOrder("RO10GSKF"),
-            new RepairOrder("ROLWERS")
-
+            new RepairOrder("RO1", "Battery does not charge", "0701234567", "BIKE100"),
+            new RepairOrder("RO2", "Brakes need adjustment", "0737654321", "BIKE200"),
+            new RepairOrder("RO3", "Motor makes strange noise", "0761112233", "BIKE300")
         };
     }
 
@@ -51,6 +49,17 @@ public class RepairOrderRegistry {
      * @param bikeSerialNo The bike serial number.
      */
     public void createRepairOrder(String problemDescr, String customerPhone, String bikeSerialNo) {
+        String repairOrderId = "RO" + (repairOrders.length + 1);
+        RepairOrder newRepairOrder = new RepairOrder(repairOrderId, problemDescr, customerPhone, bikeSerialNo);
+
+        RepairOrder[] updatedRepairOrders = new RepairOrder[repairOrders.length + 1];
+
+        for (int i = 0; i < repairOrders.length; i++) {
+            updatedRepairOrders[i] = repairOrders[i];
+        }
+
+        updatedRepairOrders[updatedRepairOrders.length - 1] = newRepairOrder;
+        repairOrders = updatedRepairOrders;
     }
 
     /**
@@ -59,6 +68,12 @@ public class RepairOrderRegistry {
      * @param repairOrder The repair order to update.
      */
     public void updateRepairOrder(RepairOrder repairOrder) {
+        for (int i = 0; i < repairOrders.length; i++) {
+            if (repairOrders[i].getRepairOrderId().equals(repairOrder.getRepairOrderId())) {
+                repairOrders[i] = repairOrder;
+                return;
+            }
+        }
     }
 
     /**
