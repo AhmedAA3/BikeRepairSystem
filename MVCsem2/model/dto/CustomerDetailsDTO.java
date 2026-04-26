aea05ecf90e0fc7ff8070112b0fefc5ab46d99b6
Michael Leniker <michael.leniker@hotmail.com>
2026-04-26 16:42:35 +0200

    Updated CustomerDetailsDTO

--- MVCsem2/model/dto/CustomerDetailsDTO.java
+++ MVCsem2/model/dto/CustomerDetailsDTO.java
@@ -2,10 +2,25 @@ package MVCsem2.model.dto;
 
 import MVCsem2.model.entity.Customer;
 
+/**
+ * Innehåller detaljer om en kund. Detta är en immutable databärare.
+ */
 public class CustomerDetailsDTO {
+    private final String name;
+    private final String email;
+    private final String phone;
 
-	public CustomerDetailsDTO CustomerDetailsDTO(Customer customer) {
-		return null;
-	}
+    /**
+     * Skapar en ny instans baserat på en kund-entitet.
+     * @param customer Kunden som data ska hämtas ifrån.
+     */
+    public CustomerDetailsDTO(Customer customer) {
+        this.name = customer.getName();
+        this.email = customer.getEmail();
+        this.phone = customer.getPhoneNumber();
+    }
 
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getPhone() { return phone; }
 }
